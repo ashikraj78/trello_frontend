c9bf0019286bd45c126e1d8d7a32082102e2a373
ashikraj78 <ashikraj.78@gmail.com>
2020-12-16 14:03:55 +0530

    Updates

---
+++ precache-manifest.289dc5d91e1a322221130abd352b5917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f479156fd303165b996001652f1abb73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b87c11964e0d9f229a4",
+    "url": "/static/css/main.4872f1ad.chunk.css"
+  },
+  {
+    "revision": "e781f0ac9b7e0c8e2a93",
+    "url": "/static/js/2.78463f8e.chunk.js"
+  },
+  {
+    "revision": "ee9d6ff43844efbe4ddeab3c8d2b69b8",
+    "url": "/static/js/2.78463f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b87c11964e0d9f229a4",
+    "url": "/static/js/main.73a2c966.chunk.js"
+  },
+  {
+    "revision": "1fb2a826c91d492fe8fd",
+    "url": "/static/js/runtime-main.417bca91.js"
+  }
+]);
